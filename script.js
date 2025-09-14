4802bafa78c16955d988e27879e282c8341c44fb
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-14 19:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+
+
+ function getRandomColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+ }
+
+ for(let i=1;i<=800;i++){
+    const box = document.createElement("div");
+    box.classList.add("square");
+    container.appendChild(box);
+
+    box.addEventListener("mouseover",()=>{
+           const randomColor = getRandomColor();
+        box.style.backgroundColor =randomColor;
+
+        setTimeout(()=>{
+            box.style.backgroundColor = "rgb(43, 33, 33)";
+        }, 1000);
+    });
+container.appendChild(box);
+ }
